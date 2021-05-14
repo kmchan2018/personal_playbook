34048d2c24fef878fedeec96bbe997bcad65a595
Chan Kwun Man <kmchan3@gmail.com>
2021-05-14 17:54:28 +0800

    Golang Configuration

---
+++ files/home/kmchan/.profile.d/golang.sh
@@ -0,0 +1,15 @@
+
+#
+# Move the go directory away from the default location directly under the
+# home directory, making the home directory less cluttered.
+#
+
+export GOPATH=/home/kmchan/.local/lib/go
+
+#
+# Extend the PATH environment variable to cover where the golang binaries
+# are installed.
+#
+
+export PATH=$PATH:/home/kmchan/.local/lib/go/bin
+
